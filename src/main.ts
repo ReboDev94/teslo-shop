d3f437e02c663e5dd6fcfb52d0c01d502dfb0572
Rafael de Jesus <rrrrebolledohdz@gmail.com>
2023-02-05 12:34:59 -0600

    added swagger config

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,21 @@
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
+
+  /* swagger */
+  const config = new DocumentBuilder()
+    .setTitle('Teslo RESTAPI')
+    .setDescription('Teslo Shop')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
